6def207892e0e8a932eebc957dcb7d3c07b09dd1
Spoffy <spoffeh@gmail.com>
2016-05-09 20:00:18 +0100

    Switched to wall prefab that alters the Nav Mesh

--- Assets/Scripts/RoomCreator.cs
+++ Assets/Scripts/RoomCreator.cs
@@ -2,20 +2,30 @@
 using System.Collections;
 
 public class RoomCreator : MonoBehaviour {
+    public GameObject wallBlock;
     public Vector3 origin;
     public float cellSizeInWorldUnits = 1;
     public float wallThickness = 0.1f;
     public float wallHeight = 3f;
 
+    private Vector3 northWallOffset;
+    private Vector3 westWallOffset;
     private RoomGenerator generator = new RoomGenerator(10);
 
 	// Use this for initialization
 	void Start () {
+        calculateOffsets();
         generator.createMap();
         generator.mapSize = 2;
         generator.map = new RoomGenerator.GridState[2,2] { { RoomGenerator.GridState.NorthWall, RoomGenerator.GridState.NorthWall}, { RoomGenerator.GridState.WestWall, RoomGenerator.GridState.WestWall} };
         spawnRoom();
 	}
+
+    void calculateOffsets()
+    {
+        northWallOffset = new Vector3(0, wallHeight/2, -cellSizeInWorldUnits / 2);
+        westWallOffset = new Vector3(-cellSizeInWorldUnits / 2, wallHeight/2, 0);
+    }
 
     Vector3 gridToWorldPosition(int x, int y)
     {
@@ -40,16 +50,16 @@ public class RoomCreator : MonoBehaviour {
 
     GameObject createWall(int x, int y, RoomGenerator.GridState wallDirection)
     {
-        GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        GameObject wall = Instantiate(wallBlock);
         if ((wallDirection & RoomGenerator.GridState.NorthWall) == RoomGenerator.GridState.NorthWall) {
             Vector3 wallDimensions = new Vector3(cellSizeInWorldUnits, wallHeight, wallThickness);
             wall.transform.localScale = wallDimensions;
-            wall.transform.position = gridToWorldPosition(x, y) + new Vector3(0, 0, -cellSizeInWorldUnits / 2);
+            wall.transform.position = gridToWorldPosition(x, y) + northWallOffset;
         } else
         {
             Vector3 wallDimensions = new Vector3(wallThickness, wallHeight, cellSizeInWorldUnits);
             wall.transform.localScale = wallDimensions;
-            wall.transform.position = gridToWorldPosition(x, y) + new Vector3(-cellSizeInWorldUnits / 2, 0, 0);
+            wall.transform.position = gridToWorldPosition(x, y) + westWallOffset;
         }
         return wall;
     }
